c5e145e4364dbcb65f7b4d9854788c63ec92d4b8
Sebastian Schmittner <schmittner@eecc.info>
2020-06-17 21:57:47 +0200

    spring boiler plate

---
+++ src/main/java/de/echsecutables/rollandbuild/RollAndBuildApplication.java
@@ -0,0 +1,13 @@
+package de.echsecutables.rollandbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollAndBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollAndBuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/echsecutables/rollandbuild/RollAndBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package de.echsecutables.rollandbuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RollAndBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
